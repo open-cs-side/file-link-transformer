bb37739de490903f8a931aeab5faaabfce5eb786
csbiy <katd6@naver.com>
2023-09-16 22:09:16 +0900

    initial commit

---
+++ src/main/java/open/filelink/FilelinkApplication.java
@@ -0,0 +1,13 @@
+package open.filelink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilelinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilelinkApplication.class, args);
+	}
+
+}
